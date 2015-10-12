2550424550dd1a47ac10bb64ca98b236c407e9a1
yanhuachn <yanhuachn@163.com>
2015-10-12 16:20:31 -0400

    just a test

--- app/src/main/java/com/example/apple/loginui/GameActivity.java
+++ app/src/main/java/com/example/apple/loginui/GameActivity.java
@@ -11,6 +11,7 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        //change test.
     }
 
     @Override
